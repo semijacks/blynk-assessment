a5ec1b79c7db7c939163d968caf61aa4ba245c70
Semilore Idowu <semijacks27@gmail.com>
2022-08-27 16:29:23 +0100

    mongo and logging setup

---
+++ api/src/server.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express'
+import http from 'http'
+import mongoose from 'mongoose'
+import { config } from './config/config'
+import Logging from './library/Logging'
+
+const router = express()
+
+mongoose
+  .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+  .then(() => {
+    Logging.info('Connected to mongoDB')
+  })
+  .catch((error) => {
+    Logging.error(`Unable to connect: `)
+    Logging.error(`${error}`)
+  })
